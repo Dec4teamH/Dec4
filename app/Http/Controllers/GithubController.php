4ec3f74b493df541079db3ca9f8d4b4fcd84dcb3
shutouyusei <shu-yusei-0711@docomo.ne.jp>
2023-01-24 21:31:06 +0900

    [add]Repositoriesにデータを格納 fix_timezone関数をGithubController内に追加

--- app/Http/Controllers/GithubController.php
+++ app/Http/Controllers/GithubController.php
@@ -12,6 +12,19 @@ use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth as FacadesAuth;
 
+
+// githubapiから取得したじかんをDBに格納できるtimestampがたに変換
+// 0000-00-00T00:00:00Zを日本時間に直すかは考える
+function fix_timezone($timestamp){
+    $year=mb_substr($timestamp,0,4);
+    $month=mb_substr($timestamp,5,2);
+    $day=mb_substr($timestamp,8,2);
+    $hour=mb_substr($timestamp,11,2);
+    $min=mb_substr($timestamp,14,2);
+    $sec=mb_substr($timestamp,17,2);
+    $fixed_time=$year."-".$month."-".$day." ".$hour.":".$min.":".$sec;
+    return $fixed_time;
+}
 // curlの情報をjson形式でreturn 
 function httpRequest($curlType, $url, $params = null, $header = null)
                             {
@@ -49,9 +62,8 @@ class GithubController extends Controller
     public function index()
     {
         // この下のアクセストークンは今後DBから取り出すが、今はDBがないので自分で打ち込む
-        $access_token="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";
+        $access_token="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";
   // DBから登録したアクセストークンをもとに登録したgithubのアカウントを表示
-
 //   下で手に入る情報もstoreのときにDBに格納して、毎回apiで情報をとるのではなくDBから取り出す
         return view('dashboard');
     }
@@ -110,6 +122,11 @@ class GithubController extends Controller
 // Gh_ profiles
         $result= Gh_profiles::create(['id'=>$resJsonUser['id'],'acunt_name'=>$resJsonUser['login'],'access_token'=>$access_token]);
 //  Repositories
+        foreach($resJsonRepos as $resJsonRepo){
+                $result=Repositories::create(['id'=>$resJsonRepo['id'],'gh_account_id'=>$resJsonUser['id'],'repos_name'=>$resJsonRepo['name'],'owner_id'=>$resJsonRepo['owner']['id'],'owner_name'=>$resJsonRepo['owner']['login'],
+                'created_date'=>fix_timezone($resJsonRepo['created_at'])
+                ]);
+}
         return redirect()->route("dashboard.index");
 }
 
