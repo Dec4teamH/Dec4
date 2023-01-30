4b8340ad198e0ec749d94e045dd5de9f1c2a67f4
shutouyusei <shu-yusei-0711@docomo.ne.jp>
2023-01-30 23:39:06 +0900

    [add]矢印の回転の挙動を正しいものに、矢印が虹色に描画

--- public/js/graph.js
+++ public/js/graph.js
@@ -10,16 +10,70 @@ let h = document.getElementById('canvas').clientHeight;
 let H = h / 2;
 let W = w / 2;
 
-// 円
-context.beginPath();
-context.arc(W, H, H-30, 0, 2*Math.PI);
+   // 円
+    context.beginPath();
+    context.arc(W, H, H - 30, 0, 2 * Math.PI);
 context.stroke();
-let theta = 1.5*Math.PI;
-// 矢印
-let arow_s_w = W + (H - 30) * Math.sin(theta);
-let arow_s_h = H - (H - 30) * Math.cos(theta);
-context.beginPath();
-context.moveTo(arow_s_w,arow_s_h);
-context.lineTo(arow_s_w+20*Math.sin((11*Math.PI/6)+theta),arow_s_h - 20*Math.cos((11*Math.PI/6)+theta));
-context.lineTo(arow_s_w+20*Math.sin((7*Math.PI/6)+theta),arow_s_h - 20*Math.cos((7*Math.PI/6)+theta));
-context.fill();
+    let r = 5;
+    let g = 3;
+    let b = 1;
+    let tm = 0;
+    let t = 0;
+    // 255>かの判別
+    let r_tell = true;
+    let g_tell = true;
+    let b_tell = true;
+const step = () => {
+    // 色の処理
+    if (t > 10) {
+        
+        if (r === 255||r===0) {
+            r_tell = !r_tell;
+        } 
+        
+        if (r_tell) {
+            r++; r++; r++; r++; r++;
+        }
+        else {
+            r--; r--; r--; r--; r--;
+        }
+
+        if (g === 255||g===0) {
+            g_tell = !g_tell;
+        } 
+        
+        if (g_tell) {
+            g++;g++;g++;
+        }
+        else {
+            g--;g--;g--;
+        }
+
+        if (b === 255||b===0) {
+            b_tell = !b_tell;
+        } 
+        
+        if (b_tell) {
+            b++; 
+        }
+        else {
+            b--; 
+        }
+        t = 0;
+    }
+    console.log(r, g, b);  
+    context.fillStyle = `rgba(${r}, ${g}, ${b}, 1)`;
+    // 矢印
+    let theta = (tm/720) * Math.PI;
+    let arow_s_w = W + (H - 30) * Math.sin(theta);
+    let arow_s_h = H - (H - 30) * Math.cos(theta);
+    context.beginPath();
+    context.moveTo(arow_s_w, arow_s_h);
+    context.lineTo(arow_s_w + 20 * Math.sin((11 * Math.PI / 6) + theta), arow_s_h - 20 * Math.cos((11 * Math.PI / 6) + theta));
+    context.lineTo(arow_s_w + 20 * Math.sin((7 * Math.PI / 6) + theta), arow_s_h - 20 * Math.cos((7 * Math.PI / 6) + theta));
+    context.fill();
+    tm++;
+    t++;
+    requestAnimationFrame(step);
+};
+requestAnimationFrame(step);
