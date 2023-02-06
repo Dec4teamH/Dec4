f28cf2ff0b3def07210c395fe9558f20904e3dcf
shutouyusei <shu-yusei-0711@docomo.ne.jp>
2023-02-06 23:32:34 +0900

    [add]fullcalendarの設定

--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -1,15 +1,26 @@
 import { Calendar } from '@fullcalendar/core'
 import timeGridPlugin from '@fullcalendar/timegrid'
+import scrollgrid from '@fullcalendar/scrollgrid';
 
 var calendarEl = document.getElementById("calendar");
 
 const calendar = new Calendar(calendarEl, {
-    plugins: [timeGridPlugin],
+    plugins: [timeGridPlugin], 
     initialView: 'timeGridDay',
+    allDaySlot: false,
+    height: 600,
+    local:"ja",
     headerToolbar: {
         left: 'prev,next',
         center: 'title',
         right: 'timeGridDay,timeGridWeek' // user can switch between the two
+    }, 
+    slotLabelFormat: {
+        hour: 'numeric',
+        minute: '2-digit',
+        omitZeroMinute: false,
+        meridiem: 'short'
     },
+
 });
 calendar.render();
