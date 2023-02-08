879728d152eb17b271598e4e04bcad489efba0e2
taketakebamboo <takeuguisu@gmail.com>
2023-02-08 22:26:23 +0900

    [update] fix migrate file repositories issues commits pullrequests migrate success

--- app/Models/Issues.php
+++ app/Models/Issues.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Issues extends Model
 {
     use HasFactory;
+
+    protected $table = 'issues';
+
+    protected $guarded = ['created_at', 'updated_at'];
+
+    // 主キーのカラム名指定
+    protected $primaryKey = 'id';
+    // オートインクリメント無効化
+    public $incrementing = false;
+    // データ型を指定h
+    protected $KeyType = 'integer';
 }
--- app/Models/Pullrequests.php
+++ app/Models/Pullrequests.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Pullrequests extends Model
 {
     use HasFactory;
+
+    protected $table = 'pullrequests';
+    protected $guarded = ['created_at', 'updated_at'];
+
+    // 主キーのカラム名指定
+    protected $primaryKey = 'id';
+    // オートインクリメント無効化
+    public $incrementing = false;
+    // データ型を指定h
+    protected $KeyType = 'integer';
 }
