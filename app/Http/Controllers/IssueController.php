2e4db9f6b50324eb64abc26018d3cc0fe2ff4513
taketakebamboo <takeuguisu@gmail.com>
2023-02-18 19:08:14 +0900

    [update] access localhost/issue see issue title list

---
+++ app/Http/Controllers/IssueController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use App\Models\Gh_profiles;
+use App\Models\Gh_accounts;
+use App\Models\Repositories;
+use App\Models\Issues;
+use App\Models\Commits;
+use App\Models\Pullrequests;
+use Dotenv\Validator as DotenvValidator;
+use App\Models\User;
+use Auth;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
+use DB;
+
+
+class IssueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $issues = DB::table('issues')->get();
+        //ddd($issues);
+        return view('Gitissue_view',['issues'=>$issues]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return view('Gitissue_view');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/Gitissue_view.blade.php
+++ resources/views/Gitissue_view.blade.php
@@ -8,7 +8,19 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div id="calendar"></div>
+                <div>
+              @foreach ($issues as $issue)
+              <tr class="hover:bg-grey-lighter">
+                <td class="py-4 px-6 border-b border-grey-light">
+                  <h3 class="text-left font-bold text-lg text-grey-dark">{{ $issue->title }}</h3>
+                  <div class="flex">
+                    <!-- 更新ボタン -->
+                    <!-- 削除ボタン -->
+                  </div>
+                </td>
+              </tr>
+              @endforeach
+                </div>
             </div>
         </div>
     </div>
