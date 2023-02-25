6197c2b7b16839b325affd1a8d15119a344a072c
shutouyusei <shu-yusei-0711@docomo.ne.jp>
2023-02-25 21:00:46 +0900

    [fix]applicationの言語などの設定を調整

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <title>GH Tracker</title>
+    <link rel="shortcut icon" href="/images/Dec4_logo2.png">
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
